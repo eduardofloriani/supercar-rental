0fcf293a5faa5e1e0b68ab2516ac2b4ed66c3e5d
eduardofloriani <eeduardofloriani@gmail.com>
2023-04-12 15:36:41 -0300

    inital structure

---
+++ src/main/java/eduardofloriani/supercarrental/SupercarRentalApplication.java
@@ -0,0 +1,13 @@
+package eduardofloriani.supercarrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupercarRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupercarRentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eduardofloriani/supercarrental/SupercarRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package eduardofloriani.supercarrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupercarRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
